15c01de1b5d24334387b18e22bc8cb4c2dd357b0
Sumit Paul <pauls1317@gmail.com>
2024-03-26 21:27:27 +0530

    string slicing

--- revision-book/02-data-types/01-strings.py
+++ revision-book/02-data-types/01-strings.py
@@ -39,3 +39,27 @@ print('This is {n} and I am {a} years old.'.format(n = name, a = age))
 ##* formatted strings - starts with f followed by string
 # using variables inside string directly
 print(f'Hello {name} your age is {age}')
+
+
+##* String slicing - selecting sub string
+# Syntax: [start : stop : step] returns string slice from start(inclusive) index to stop(exclusive) index
+
+name = 'Stephen'
+
+print(name[0 : 3]) # Ste
+# left empty start and stop -> gives complete string
+print(name[ : ]) # Stephen
+
+print(name[0 : 4]) # Step
+
+# using negative index
+print(name[-1:]) # n - last character
+
+# with step argument
+print(name[0 : 4 : 2]) # Se
+
+# reversing string using -ve step index
+print(name[::-1]) # nehpetS
+print(name[-1::-1]) # nehpetS
+
+print(name[-1: -(len(name) + 1) : -1]) # nehpetS
