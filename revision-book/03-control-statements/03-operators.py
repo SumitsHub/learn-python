ffd3e6690159a9d9639b234559ee6e0f04e00c25
Sumit Paul <pauls1317@gmail.com>
2024-03-27 09:38:12 +0530

    'in' operator

--- revision-book/03-control-statements/03-operators.py
+++ revision-book/03-control-statements/03-operators.py
@@ -1,6 +1,7 @@
 ## Operators used with conditional statements
 
 ##* 'and' and 'or' operators
+# NOTE: && and || operators are not available in python
 
 age = int(input('Enter your age to proceed: '))
 
@@ -14,3 +15,28 @@ if age < 18 or age > 50:
     print('Sorry! You can\'t proceed' )
 else:
     print('You can go ahead!')
+
+
+
+##* 'in' keyword - returns true if left operand is present in the right operand
+
+# check in string
+if "a" in "Capital":
+    print("a is present")  # it's the output
+else:
+    print("a is not there")
+
+# NOTE: 'in' operator is case-sensitive
+if "cat" in ["Dog", "Cat", "Elephant"]:
+    print("cat found")
+else:
+    print("cat not found")  # -> output
+
+
+# check in list
+if 3 in [1, 2, 3, 4]:
+    print("3 is present")  # it's the output
+
+if "apple" in ["banana", "apple", "orange", "pomogrenate"]:
+    print("apple found")  # -> output
+   
