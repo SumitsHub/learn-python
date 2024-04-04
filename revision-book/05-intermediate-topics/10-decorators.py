5d7547aaab22de129b6257ea5eb489956fb986f0
Sumit Paul <pauls1317@gmail.com>
2024-04-04 13:15:32 +0530

    @wraps from functools

--- revision-book/05-intermediate-topics/10-decorators.py
+++ revision-book/05-intermediate-topics/10-decorators.py
@@ -86,3 +86,27 @@ def test_func():
 
 print(test_func.__name__)  # wrapper
 print(test_func.__doc__)  # This is wrapper function doc
+
+
+
+##* @wraps
+# It's a helper function which preserves metadata such as the function's name, docstring, and other attributes.
+from functools import wraps
+
+def my_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f'You are calling {func.__name__}()') # You are calling add()
+        print(f'{func.__doc__}')  # This function adds 2 input numbers and returns the result
+        return func(*args, **kwargs)
+    return wrapper
+
+@my_decorator
+def add(a, b):
+    """This function adds 2 input numbers and returns the result"""
+    return a + b
+
+
+print(add(2, 4))  # 6
+print(add.__name__)  # add
+print(add.__doc__)  # This function adds 2 input numbers and returns the result
